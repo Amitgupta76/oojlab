2b46a4fc14f840950243dcca7885a29767d797f7
Amitgupta76 <84084490+Amitgupta76@users.noreply.github.com>
2021-10-22 12:31:37 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,10 @@
+class add
+{
+public static void main(String args[])
+{
+int num;
+num=100;
+System.out.println("this is num:"+num);
+num =num*2;
+System.out.println("the value of numis"+num);
+}} 
---
+++ oddeven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class oddeven
+{
+public static void main(String args[])
+{
+int a;
+Scanner in=new Scanner(System.in);
+System.out.println("enter the integer");
+a=in.nextInt();
+if(a%2==0)
+System.out.println("even number");
+else 
+System.out.println("odd number");
+}}
